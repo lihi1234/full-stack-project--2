571a32e10279fa7d4ae754453b03c0cfa085e3a3
HEH12 <126557630+HEH12@users.noreply.github.com>
2024-02-19 23:55:21 +0200

    block user

--- js/login.js
+++ js/login.js
@@ -1,3 +1,7 @@
+let trys=0;
+let f = document.getElementById("loginForm");
+var messageElement = document.getElementById('message');
+
 function validateLogin() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
@@ -21,10 +25,20 @@ function validateLogin() {
             window.location.href = "games.html";
             // You can redirect to another page or perform additional actions here
         } else {
+            trys++;
             alert('Invalid password. Please try again.');
         }
     } else {
+        trys++;
         alert('User not found. Please sign up.');
     }
-    
+     if(trys>=5)
+    {
+        f.style.display='none';
+        alert("you are block for 3 minutes")
+        messageElement.style.display = 'block'; // הצגת ההודעה
+        setTimeout(function() {
+            window.location.href = 'welcome.html';}, 180000); // 180000 מייצג 3 דקות במילי-שניות (60 שניות * 3)
+
+    }
 }
