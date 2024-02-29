a0907493f75ded7ad12a5dd905bf762b0dd46f6b
ליהי ברלב <lihibl5@gmail.com>
2024-02-29 14:08:39 +0200

    ss

--- js/connect4.js
+++ js/connect4.js
@@ -15,7 +15,10 @@ function check(player) {
 
         for (i = 1; i <= 7; i++) {
             for (j = 1; j <= 3; j++) {
-                if (document.getElementById(`c${i}r${j}`).style.backgroundColor == `${player}` && document.getElementById(`c${i}r${j + 1}`).style.backgroundColor == `${player}` && document.getElementById(`c${i}r${j + 2}`).style.backgroundColor == `${player}` && document.getElementById(`c${i}r${j + 3}`).style.backgroundColor == `${player}`) {
+                if (document.getElementById(`c${i}r${j}`).style.backgroundColor == `${player}` &&
+                     document.getElementById(`c${i}r${j + 1}`).style.backgroundColor == `${player}` && 
+                        document.getElementById(`c${i}r${j + 2}`).style.backgroundColor == `${player}` && 
+                            document.getElementById(`c${i}r${j + 3}`).style.backgroundColor == `${player}`) {
                     alert(`${player} wins`)
                     update()
                     location.reload()
@@ -26,7 +29,10 @@ function check(player) {
 
         for (i = 1; i <= 6; i++) {
             for (j = 1; j <= 4; j++) {
-                if (document.getElementById(`c${j}r${i}`).style.backgroundColor == `${player}` && document.getElementById(`c${j + 1}r${i}`).style.backgroundColor == `${player}` && document.getElementById(`c${j + 2}r${i}`).style.backgroundColor == `${player}` && document.getElementById(`c${j + 3}r${i}`).style.backgroundColor == `${player}`) {
+                if (document.getElementById(`c${j}r${i}`).style.backgroundColor == `${player}` && 
+                    document.getElementById(`c${j + 1}r${i}`).style.backgroundColor == `${player}` && 
+                        document.getElementById(`c${j + 2}r${i}`).style.backgroundColor == `${player}` &&
+                             document.getElementById(`c${j + 3}r${i}`).style.backgroundColor == `${player}`) {
                     alert(`${player} wins`)
                     update()
                     location.reload()
@@ -38,7 +44,10 @@ function check(player) {
 
         for (i = 1; i <= 4; i++) {
             for (j = 1; j <= 3; j++) {
-                if (document.getElementById(`c${i}r${j}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 1}r${j + 1}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 2}r${j + 2}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 3}r${j + 3}`).style.backgroundColor == `${player}`) {
+                if (document.getElementById(`c${i}r${j}`).style.backgroundColor == `${player}` &&
+                     document.getElementById(`c${i + 1}r${j + 1}`).style.backgroundColor == `${player}` &&
+                         document.getElementById(`c${i + 2}r${j + 2}`).style.backgroundColor == `${player}` && 
+                            document.getElementById(`c${i + 3}r${j + 3}`).style.backgroundColor == `${player}`) {
                     alert(`${player} wins`)
                     update()
                     location.reload()
@@ -49,7 +58,10 @@ function check(player) {
 
         for (i = 1; i <= 4; i++) {
             for (j = 6; j >= 4; j--) {
-                if (document.getElementById(`c${i}r${j}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 1}r${j - 1}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 2}r${j - 2}`).style.backgroundColor == `${player}` && document.getElementById(`c${i + 3}r${j - 3}`).style.backgroundColor == `${player}`) {
+                if (document.getElementById(`c${i}r${j}`).style.backgroundColor == `${player}` && 
+                    document.getElementById(`c${i + 1}r${j - 1}`).style.backgroundColor == `${player}` && 
+                        document.getElementById(`c${i + 2}r${j - 2}`).style.backgroundColor == `${player}` && 
+                            document.getElementById(`c${i + 3}r${j - 3}`).style.backgroundColor == `${player}`) {
                     alert(`${player} wins`)
                     update()
                     location.reload()
